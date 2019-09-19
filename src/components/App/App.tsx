b5dab0555b7a50912309033a7294fcaf3799cfb2
eastend-street <ss_1322irf@icloud.com>
2019-09-19 15:30:03 -0700

    added reset style of styled-components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
+import { Reset } from 'styled-reset'
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
+      <Reset />
       <Header />
       <Footer />
     </React.Fragment>
