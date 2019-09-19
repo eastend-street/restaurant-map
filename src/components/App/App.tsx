90ac81db134fff8f9b0ad45a811e5a1aeea5ffcb
eastend-street <ss_1322irf@icloud.com>
2019-09-19 15:17:08 -0700

    added header and footer

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/header';
+import Footer from '../Footer/footer';
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+      <header className="App-header">
+          header is here
+      </header>
+  );
+}
+
+export default Header;
